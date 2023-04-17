2154dbcda376f2c5838d7922a8ec119af5a68d05
Cristhian-Chavez <crsth.1990@fpuna.edu.py>
2023-04-16 20:41:58 -0400

    update method

--- api/server.js
+++ api/server.js
@@ -33,12 +33,18 @@ app.post('/todo/new', (req, res) => {
 
 app.delete('/todo/delete/:id', async (req, res) => {
     const result = await Todo.findByIdAndDelete(req.params.id);
-})
 
-app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id);
-
-    res.json({result});
+    res.json(result);
 });
+
+app.put('/todo/complete/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    todo.complete = !todo.complete;
+
+    todo.save();
+
+    res.json(todo);
+})
 
 app.listen(3001)
